46a0d4b09879ec6b4159c7de63cdfb973b42747f
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2021-05-14 16:22:29 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4_SGoQQStOJ3wuY2xHnYs_xiLluULhng",
+    authDomain: "credit-card-fabricio.firebaseapp.com",
+    projectId: "credit-card-fabricio",
+    storageBucket: "credit-card-fabricio.appspot.com",
+    messagingSenderId: "248627508982",
+    appId: "1:248627508982:web:13343904dd088e70f71ed7"
+  }
+  
+};
